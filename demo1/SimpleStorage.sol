8c0b6d339cf1dda632c49ab7042a6ac87fe3d58f
Rahul Gupta <rg5353070@gmail.com>
2024-08-31 20:57:51 +0530

    Created a storage factory

---
+++ demo1/SimpleStorage.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage{
+    // boolean, uint, int, address bytes
+    bool hasFavoriteNumber = false;
+    // defining an array of strings
+    string[] public names;
+    uint  favoriteNumber;
+
+    // the default visibility is internal means can only be called by the contract 
+    string TextNumber = "five";
+
+    // a dict that maps strings to numbers
+    mapping(string => uint256) public nameToFavoriteNumber;
+
+    address myAddress = 0x8e7E00d5ba604699B11F69C459b1A01174d6c5b4;
+    bytes32 myBytes = "cat";
+
+    // People public person1 = People({favoriteNumber:69, name:"Rahul"});
+
+    People[] persons;
+
+    // creating a custom type
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+        string designation;
+    }
+
+
+    function store(uint256 _favoriteNum) public {
+        favoriteNumber = _favoriteNum;
+    }
+
+
+    // a view function/keyword implies that we are only going to read state from this function and not do anything
+
+    // view and pure function disallow modification of state
+
+    function retrieve() public view returns(uint256){
+        return favoriteNumber;
+    }
+
+
+    // just pure function dont allow modification or reading just for performing some abstract calculation and returning it
+    function add() public pure returns(uint256){
+        return (1+1);
+    }
+
+    // calldata, memory, storage
+
+    function addPeople(string memory _name, uint256 _favoriteNumber, string calldata _designation)
+     public {
+
+        // we cannot modify calldata arguements
+        // _designation = "helloWorld";
+
+        // since we dont modify this name variable after this function, we can keep it as memory or even calldata
+
+        _name = "string";
+
+        People memory newPerson = People({favoriteNumber:_favoriteNumber,name: _name,designation:_designation});
+        persons.push(newPerson);
+    }
+
+
+      // calldata, memory, storage
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        People memory newPerson = People({favoriteNumber:_favoriteNumber,name: _name, designation:"someone"});
+        persons.push(newPerson);
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
---
+++ demo1/StorageFactory.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory{
+
+    // creating a contract from other one
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber)public{
+        // address
+        // ABI - application binary interface
+        // The ABI is a JSON format that describes the interface of a smart contract, including its functions, inputs, outputs, and events.
+
+        // getting a contract from the array
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];    
+
+        // calling a method from it
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+     function sgGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        SimpleStorage simpleStorage = simpleStorageArray[_simpleStorageIndex];
+        return simpleStorage.retrieve();
+    }
+
+}
