694ed8898189791ebf477c1e01b2f3b5ce6c014d
Rahul Gupta <rg5353070@gmail.com>
2024-09-01 15:07:42 +0530

    added recieve and fallback

--- demo1/FundMe.sol
+++ demo1/FundMe.sol
@@ -1,43 +1,62 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Get funds from users
+// withdraw funds, make sure that the person withdrawing is the owner
+// set a minimum funding value in USD
+
 import "./PriceConverter.sol";
 
+// creating custom errors
+error NotOwner();
 
-contract FundMe{
-
+contract FundMe {
     using PriceConverter for uint256;
-
-    uint256 minimumUsd = 50 * 1e18;
+    address public immutable i_contractOwner;
+    uint256 public constant MINIMUM_USD = 50 * 1e18;
 
     // address of the people who send us money
-    address[]  public funders;
+    address[] public funders;
     mapping(address => uint256) public adddressToAmountFunded;
 
-    function fund () public payable  {
+    constructor() {
+        i_contractOwner = msg.sender;
+    }
+
+    function fund() public payable {
         // 1e8 is shorthand for 1×1081×108, which is 1 followed by 8 zeros, or 100,000,000.
         // require is a check
         // which makes sure that the condition passes
-        require(msg.value.getConversionRate() >= minimumUsd,"send atleast 1ETH");   
+        require(
+            msg.value.getConversionRate() >= MINIMUM_USD,
+            "send atleast 1ETH"
+        );
         // msg.value -> 18 decimals
         funders.push(msg.sender);
-        adddressToAmountFunded[msg.sender] = msg.value;
+        adddressToAmountFunded[msg.sender] += msg.value;
     }
 
+    modifier onlyOwner() {
+        // require(
+        //     msg.sender == i_contractOwner,
+        //     "only the owner can withdraw funds"
+        // );
+        if(msg.sender != i_contractOwner){revert NotOwner();}
+        _;
+    }
 
-
-   function withdraw () public {
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) 
-        {
-          address funder = funders[funderIndex];
-          adddressToAmountFunded[funder] = 0;
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            adddressToAmountFunded[funder] = 0;
         }
-
-
-        // reset the array 
+        // reset the array
 
         funders = new address[](0);
-
         //witdraw the funds
 
         // three different ways of sending ether
@@ -45,15 +64,35 @@ contract FundMe{
         // send
         // call
 
-
         // msg.sender -> address
         // payable(msg.sender) -> payabale address
 
         // transfer throws an error
-        payable(msg.sender).transfer(address(this).balance);
+        // payable(msg.sender).transfer(address(this).balance);
 
-        // send does not throw an error
-        payable(msg.sender).send(address(this).balance);
+        // // send does not throw an error
+        // bool withdrawSuccess = payable(msg.sender).send(address(this).balance);
+        // require(withdrawSuccess,"withdraw of funds failed");
 
+        // call
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "call of funds failed");
     }
+
+
+   receive() external payable { 
+        fund();
+    }
+
+
+    fallback() external payable {
+        fund();
+    }
+
+    // what if someone sends eth without calling the fund function
+
+    // recieve()
+    // fallback()
 }
